bfe1cef549d5a005616184f6e464e8c7a5906579
ReeceFang <2656084583@qq.com>
2026-08-03 17:41:59 +0800

    latest

--- train.sh
+++ train.sh
@@ -3,12 +3,14 @@ set -e
 export OMP_NUM_THREADS=4
 
 python train.py \
-  --data-dir /root/autodl-tmp/yanxie_mask_data \
+  --data-dir /root/autodl-tmp/yanxie_data \
   --model convnext_base \
   --model-path /root/autodl-tmp/timm/convnext_base/pytorch_model.bin \
   --num-classes 20 \
   --epochs 50 \
-  --batch-size 32
+  --batch-size 32 \
+  --learning-rate 3e-4 \
+  --input-size 96
 
 echo "任务完成, 准备关机..."
 /usr/bin/shutdown
