5b735230fe01d8a126413eccaa9571d40fc166b5
ReeceFang <2656084583@qq.com>
2026-08-03 18:18:02 +0800

    latest

--- train.sh
+++ train.sh
@@ -8,8 +8,8 @@ python train.py \
   --model-path /root/autodl-tmp/timm/convnext_base/pytorch_model.bin \
   --num-classes 20 \
   --epochs 50 \
-  --batch-size 32 \
-  --learning-rate 3e-4 \
+  --batch-size 64 \
+  --learning-rate 1e-3 \
   --input-size 96
 
 echo "任务完成, 准备关机..."
