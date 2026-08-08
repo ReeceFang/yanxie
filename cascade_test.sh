820b6e968fede6c8681bcd37637c3b79922de8e3
RuimingFang <2656084583@qq.com>
2026-08-08 18:33:24 +0800

    latest

--- cascade_test.sh
+++ cascade_test.sh
@@ -2,16 +2,10 @@ set -e
 
 export OMP_NUM_THREADS=4
 
-# python cascade_test.py \
-#   --merged-run-path runs/merged \
-#   --second-runs-path runs \
-#   --val-path /root/autodl-tmp/yanxie_data/val \
-#   --mapping-json merge_dataset_classes.json \
-#   --weights best
-
-python cascade_test.py `
-  --merged-run-path runs/merged `
-  --second-runs-path runs `
-  --val-path D:/Python_Data/mmpretrain_test4/val `
-  --mapping-json merge_dataset_classes.json `
+python cascade_test.py \
+  --merged-run-path runs/merged \
+  --second-runs-path runs \
+  --val-path /root/autodl-tmp/yanxie_data/val \
+  --mapping-json merge_dataset_classes.json \
   --weights best
+  
