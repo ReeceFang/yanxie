9b1538dcd7cc2639a33145bfcd9fc1520a564292
RuimingFang <2656084583@qq.com>
2026-08-12 17:07:34 +0800

    latest

--- two_stage/extract.sh
+++
@@ -1,4 +0,0 @@
-python extract_dataset_classes.py \
-  "/root/autodl-tmp/yanxie_data" \
-  "/root/autodl-tmp/yanxie_shiying_data" \
-  "extract_dataset_classes.txt"
--- two_stage/merge.sh
+++
@@ -1,4 +0,0 @@
-python merge_dataset_classes.py \
-  "/root/autodl-tmp/yanxie_data" \
-  "/root/autodl-tmp/yanxie_merged_data" \
-  "merge_dataset_classes.json"
